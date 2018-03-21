eee98b02f8d140a1fb333360ff0500678f23c8df
sutaking <zhaofeng.vip@gmail.com>
2018-03-21 20:13:35 +0800

    add files

---
+++ main.js
@@ -0,0 +1,51 @@
+const electron = require('electron');
+const path = require('path');
+const url = require('url');
+
+const app = electron.app;
+
+const BrowserWindow = electron.BrowserWindow;
+
+
+let mainWindow
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        /*webPreferences: {
+            nodeIntegration: false
+        }*/
+        title: 'Make Your Seqeunce Diagram'
+    });
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, '../app/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    mainWindow.webContents.openDevTools();
+
+    mainWindow.on('closed', function() {
+
+        mainWindow = null;
+    });
+
+}
+
+app.on('ready', () => {
+    console.log(`--- ready ---`);
+    createWindow();
+})
+
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+})
+
+app.on('activate', function() {
+    if (mainWindow === null) {
+        createWindow();
+    }
+})
